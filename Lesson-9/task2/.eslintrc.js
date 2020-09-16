25da94215d7f9dd6c3b22e0e4dfbded9ce3a4948
Shulha Nikita <shulgamalthael@gmail.com>
2020-09-16 15:57:40 +0300

    init files

---
+++ Lesson-9/task2/.eslintrc.js
@@ -0,0 +1,15 @@
+  
+module.exports = {
+    extends: 'eslint-config-airbnb-base',
+    "rules": {
+        'no-console': 2,
+        'import/prefer-default-export': 0,
+    },
+    parserOptions: {
+        ecmaVersion: 10,
+        sourceType: 'module'
+    },
+    env: {
+        browser: true,
+    }
+};
