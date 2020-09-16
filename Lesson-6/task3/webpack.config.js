53629ca6059c9f1b73ac082e1c69af4752de4a16
Shulha Nikita <shulgamalthael@gmail.com>
2020-09-16 14:44:05 +0300

    mod

--- Lesson-6/task3/webpack.config.js
+++ Lesson-6/task3/webpack.config.js
@@ -14,7 +14,7 @@ module.exports = {
                 use: ['style-loader', 'css-loader', 'sass-loader']
             },
             {
-                test: /.(jpg|pmg)$/,
+                test: /.(jpg|png)$/,
                 use: [
                     {
                         loader: 'url-loader',
